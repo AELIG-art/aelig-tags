8d6b12129d87283c2bf8e4f52125c4ea8b3e7c88
Giacomo Chiarot <gicochiarot@gmail.com>
2024-02-25 13:48:44 +0100

    Added text

--- src/pages/Home/Disconnected.tsx
+++ src/pages/Home/Disconnected.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { Container } from "react-bootstrap";
 
 const Disconnected = () => {
-    return <Container>
-        Disconnected
-    </Container>;
+    return <div
+        className="d-flex flex-column justify-content-center align-items-center"
+        style={{height: `calc(100vh - 62px)`}}
+    >
+        <h1>Welcome to AELIG | tags</h1>
+        <h2>Connect your wallet to start</h2>
+    </div>;
 }
 
 export default Disconnected;
