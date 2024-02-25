ff827e65e61d09f36bdd6721ea781a36d70346c8
Giacomo Chiarot <gicochiarot@gmail.com>
2024-02-26 00:24:38 +0100

    Added new tags form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import NewTag from "./pages/NewTag/NewTag";
 import Layout from "./Layout";
+import { initJuno } from "@junobuild/core";
 
 function App() {
+    useEffect(() => {
+        (async () =>
+            await initJuno({
+                satelliteId: "fq2tz-xqaaa-aaaal-adv3q-cai"
+            }))();
+    }, []);
+
     const router = createBrowserRouter([
         {
             path: "/",
--- src/pages/NewTag/NewTag.tsx
+++ src/pages/NewTag/NewTag.tsx
@@ -1,9 +1,56 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { setDoc } from "@junobuild/core";
 
 const NewTag = () => {
-    return <Container>
+    const [owner, setOwner] = useState(undefined as string|undefined);
+    const [tags, setTags] = useState([] as string[]);
 
+    return <Container>
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Address</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Enter user wallet"
+                    onChange={(event) => {
+                        setOwner(event.target.value);
+                    }}
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Tag ids</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Tag id"
+                    onChange={(event) => {
+                        setTags(event.target.value.split(","));
+                    }}
+                />
+                <Form.Floating>Enter tag ids separated by a comma.</Form.Floating>
+            </Form.Group>
+            <Button
+                variant="primary"
+                onClick={() => {
+                    tags.forEach((tag) => {
+                        setDoc({
+                            collection: "tags",
+                            doc: {
+                                key: `${new Date().getTime()}`,
+                                data: {
+                                    tagId: tag,
+                                    owner,
+                                    registered: false
+                                }
+                            }
+                        }).then();
+                    });
+                }}
+            >
+                Add new tags
+            </Button>
+        </Form>
     </Container>;
 }
 
