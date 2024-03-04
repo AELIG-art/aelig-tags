4d75f508fea0b51e9f545fadd6306cc757e83954
Giacomo Chiarot <gicochiarot@gmail.com>
2024-03-04 13:16:41 +0100

    :building_construction: frontend files moved inside a folder

--- frontend/juno.config.ts
+++
@@ -1,9 +0,0 @@
-import {defineConfig} from '@junobuild/config';
-import { satelliteId } from "./src/utils/constants";
-
-export default defineConfig({
-  satellite: {
-    satelliteId,
-    source: 'build'
-  }
-});
