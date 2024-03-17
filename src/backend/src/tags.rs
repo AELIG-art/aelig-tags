ac78d58bf6cfaf241a12959218f6a8ddc91e5280
Giacomo Chiarot <gicochiarot@gmail.com>
2024-03-18 00:35:03 +0100

    ✨ Added new BE functions

--- src/backend/src/tags.rs
+++ src/backend/src/tags.rs
@@ -1,6 +1,8 @@
 use std::cell::RefCell;
+use ic_cdk::caller;
 use ic_stable_structures::{DefaultMemoryImpl, StableBTreeMap};
 use ic_stable_structures::memory_manager::{MemoryId, MemoryManager};
+use crate::admin::get_admin;
 use crate::memory_ids::MemoryKeys;
 use crate::types::{Error, Memory, Tag};
 
@@ -43,4 +45,28 @@ fn get_tags_owned_by(owner: String) -> Vec<Tag> {
             })
             .collect()
     })
+}
+
+#[ic_cdk::query]
+fn get_tags() -> Vec<Tag> {
+    TAGS.with(|tags| {
+        tags.borrow()
+            .iter()
+            .map(|(_, tag)| tag.clone())
+            .collect()
+    })
+}
+
+#[ic_cdk::update]
+fn add_tag(id: String, tag: Tag) -> Result<String, Error> {
+    return if caller() == get_admin() {
+        TAGS.with(|map| {
+            map.borrow_mut().insert(id, tag);
+        });
+        Ok("Tag inserted".to_string())
+    } else {
+        Err(Error::PermissionDenied {
+            msg: "You are not allowed".to_string()
+        })
+    }
 }
