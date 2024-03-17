049b0f2a0a7d426d88bdc3bda86fa01efcc1a3ec
Giacomo Chiarot <gicochiarot@gmail.com>
2024-03-17 12:00:30 +0100

    ✨ Admin is kept updated

--- src/backend/src/admin.rs
+++ src/backend/src/admin.rs
@@ -9,13 +9,23 @@ thread_local! {
 }
 
 #[ic_cdk::init]
-fn init() {
+fn init_admin() {
+    set_admin_to_caller();
+}
+
+#[ic_cdk::post_upgrade]
+fn set_admin() {
+    set_admin_to_caller();
+}
+
+fn set_admin_to_caller() {
     ADMIN.with(|admin| {
         let caller = api::caller();
         *admin.borrow_mut() = caller;
     });
 }
 
+#[ic_cdk::query]
 pub fn get_admin() -> Principal {
     ADMIN.with(|admin| admin.borrow().clone())
 }
