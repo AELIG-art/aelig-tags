eac4dd2d85b88e6fb47ed81478a85264702cd38d
Giacomo Chiarot <gicochiarot@gmail.com>
2024-04-01 23:13:30 +0200

    ✨ Add error content

--- src/frontend/src/pages/Verify/Error.tsx
+++ src/frontend/src/pages/Verify/Error.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Error = () => {
-    return null;
+    return <div className="container d-flex flex-column justify-content-center">
+        <div className="row justify-content-center mb-5">
+            <img src="tag-error.png" className="img-fluid" alt="" style={{ maxWidth: '500px' }} />
+        </div>
+        <div className="row">
+            <div className="col">
+                <p>
+                    The tag is not authentic or the one-time code already used. Please retry or contact <a href="mailto:support@aelig.art">support@aelig.art</a> if the error persists.
+                </p>
+            </div>
+        </div>
+    </div>;
 }
 
 export default Error;
