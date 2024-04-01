dc0545d615efcf6fdc5acd96258ad040177c8d51
Giacomo Chiarot <gicochiarot@gmail.com>
2024-04-01 23:13:01 +0200

    ✨ Add verify logic

--- src/frontend/src/pages/Verify/Verify.tsx
+++ src/frontend/src/pages/Verify/Verify.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+import {backend} from "../../declarations/backend";
+import Success from "./Success";
+import Error from "./Error";
 
 const Verify = () => {
-    return null;
+    const { msg } = useParams();
+    const [isValid, setIsValid] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        if (msg) {
+            backend.verify_tag(msg).then((res) => {
+                setIsValid("OK" in res);
+                setIsLoading(false);
+            });
+        }
+    }, [msg, setIsValid, setIsLoading]);
+
+    if (isLoading) {
+        return null;
+    } else {
+        return isValid ? <Success /> : <Error />
+    }
 }
 
 export default Verify;
