53aefa339d9af4491e90daffbbdd8f5b5f38dfdb
Giacomo Chiarot <gicochiarot@gmail.com>
2024-04-07 17:18:26 +0200

    ♻️ Refactor file

--- src/frontend/src/pages/Home/CertificatesList.tsx
+++ src/frontend/src/pages/Home/CertificatesList.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useTags } from "../../contexts/TagsContext";
-import {intToHexId} from "../../utils/conversions";
+import {intToHexId} from "../../utils/transformations";
 
 const CertificatesList = () => {
     const { tags } = useTags();
