9644720f2428a47e54c8b2a7e7c98e3d207ce58e
Giacomo Chiarot <gicochiarot@gmail.com>
2024-04-27 19:06:53 +0200

    ✨ Add SiweIdentityProvider component

---
+++ src/frontend/src/providers/SiweIdentityProvider.tsx
@@ -0,0 +1,34 @@
+import {ReactNode, useEffect} from "react";
+import {useSiweIdentity} from "ic-use-siwe-identity";
+import {useAccount} from "wagmi";
+
+const SiweIdentityProvider = (props: {
+    children: ReactNode
+}) => {
+    const {children} = props;
+
+    const { isConnected, address } = useAccount();
+
+    const {
+        clear,
+        identity,
+        identityAddress
+    } = useSiweIdentity();
+
+    useEffect(() => {
+        if (!isConnected && identity) {
+            clear();
+        }
+    }, [isConnected, clear, identity]);
+
+    useEffect(() => {
+        if (identityAddress && address && address !== identityAddress) {
+            clear();
+        }
+    }, [address, clear, identityAddress]);
+
+
+    return children;
+}
+
+export default SiweIdentityProvider;
