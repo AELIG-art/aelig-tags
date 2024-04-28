e3a02574887da3d7b5d73e761850c6b89e3a2138
Giacomo Chiarot <gicochiarot@gmail.com>
2024-04-28 21:56:59 +0200

    🔧 Add generated declarations

---
+++ src/frontend/src/declarations/backend/backend.did.d.ts
@@ -0,0 +1,71 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+import type { IDL } from '@dfinity/candid';
+
+export interface Attribute { 'trait_type' : string, 'value' : string }
+export interface Certificate {
+  'id' : bigint,
+  'owner' : string,
+  'metadata' : [] | [NFTMetadata],
+  'author' : string,
+  'registered' : boolean,
+}
+export type Error = { 'NotFound' : ErrorReason } |
+  { 'PermissionDenied' : ErrorReason } |
+  { 'Validation' : ErrorReason };
+export interface ErrorReason { 'msg' : string }
+export interface Frame { 'id' : bigint, 'nft' : [] | [NFT] }
+export type GetCertificateResult = { 'Ok' : Certificate } |
+  { 'Err' : Error };
+export type GetStorageResult = { 'Ok' : Principal } |
+  { 'Err' : Error };
+export type GetTagResult = { 'Ok' : Tag } |
+  { 'Err' : Error };
+export interface NFT {
+  'id' : string,
+  'chain' : string,
+  'contract_address' : string,
+}
+export interface NFTMetadata {
+  'name' : string,
+  'description' : string,
+  'attributes' : Array<Attribute>,
+  'image' : string,
+}
+export interface StoreArg {
+  'key' : string,
+  'content' : Uint8Array | number[],
+  'sha256' : [] | [Uint8Array | number[]],
+  'content_type' : string,
+  'content_encoding' : string,
+}
+export interface Tag {
+  'id' : bigint,
+  'owner' : string,
+  'is_certificate' : boolean,
+  'short_id' : string,
+}
+export type UpdateResult = { 'Ok' : string } |
+  { 'Err' : Error };
+export type VerifyResult = {
+    'Ok' : { 'Frame' : Frame } |
+      { 'Certificate' : Certificate }
+  } |
+  { 'Err' : Error };
+export interface _SERVICE {
+  'add_storage_canister' : ActorMethod<[Principal], UpdateResult>,
+  'add_tag' : ActorMethod<[string, Tag], UpdateResult>,
+  'get_certificate' : ActorMethod<[string], GetCertificateResult>,
+  'get_storage_principal' : ActorMethod<[string], GetStorageResult>,
+  'get_tag' : ActorMethod<[string], GetTagResult>,
+  'get_tags' : ActorMethod<[], Array<Tag>>,
+  'get_tags_owned_by' : ActorMethod<[string], Array<Tag>>,
+  'is_admin' : ActorMethod<[Principal], boolean>,
+  'register_certificate' : ActorMethod<[string], UpdateResult>,
+  'save_certificate' : ActorMethod<[string, NFTMetadata], UpdateResult>,
+  'set_key' : ActorMethod<[string, string], UpdateResult>,
+  'upload_media' : ActorMethod<[string, StoreArg], UpdateResult>,
+  'verify_tag' : ActorMethod<[string], VerifyResult>,
+}
+export declare const idlFactory: IDL.InterfaceFactory;
+export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
---
+++ src/frontend/src/declarations/backend/backend.did.js
@@ -0,0 +1,72 @@
+export const idlFactory = ({ IDL }) => {
+  const ErrorReason = IDL.Record({ 'msg' : IDL.Text });
+  const Error = IDL.Variant({
+    'NotFound' : ErrorReason,
+    'PermissionDenied' : ErrorReason,
+    'Validation' : ErrorReason,
+  });
+  const UpdateResult = IDL.Variant({ 'Ok' : IDL.Text, 'Err' : Error });
+  const Tag = IDL.Record({
+    'id' : IDL.Nat,
+    'owner' : IDL.Text,
+    'is_certificate' : IDL.Bool,
+    'short_id' : IDL.Text,
+  });
+  const Attribute = IDL.Record({ 'trait_type' : IDL.Text, 'value' : IDL.Text });
+  const NFTMetadata = IDL.Record({
+    'name' : IDL.Text,
+    'description' : IDL.Text,
+    'attributes' : IDL.Vec(Attribute),
+    'image' : IDL.Text,
+  });
+  const Certificate = IDL.Record({
+    'id' : IDL.Nat,
+    'owner' : IDL.Text,
+    'metadata' : IDL.Opt(NFTMetadata),
+    'author' : IDL.Text,
+    'registered' : IDL.Bool,
+  });
+  const GetCertificateResult = IDL.Variant({
+    'Ok' : Certificate,
+    'Err' : Error,
+  });
+  const GetStorageResult = IDL.Variant({ 'Ok' : IDL.Principal, 'Err' : Error });
+  const GetTagResult = IDL.Variant({ 'Ok' : Tag, 'Err' : Error });
+  const StoreArg = IDL.Record({
+    'key' : IDL.Text,
+    'content' : IDL.Vec(IDL.Nat8),
+    'sha256' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+    'content_type' : IDL.Text,
+    'content_encoding' : IDL.Text,
+  });
+  const NFT = IDL.Record({
+    'id' : IDL.Text,
+    'chain' : IDL.Text,
+    'contract_address' : IDL.Text,
+  });
+  const Frame = IDL.Record({ 'id' : IDL.Nat, 'nft' : IDL.Opt(NFT) });
+  const VerifyResult = IDL.Variant({
+    'Ok' : IDL.Variant({ 'Frame' : Frame, 'Certificate' : Certificate }),
+    'Err' : Error,
+  });
+  return IDL.Service({
+    'add_storage_canister' : IDL.Func([IDL.Principal], [UpdateResult], []),
+    'add_tag' : IDL.Func([IDL.Text, Tag], [UpdateResult], []),
+    'get_certificate' : IDL.Func([IDL.Text], [GetCertificateResult], ['query']),
+    'get_storage_principal' : IDL.Func(
+        [IDL.Text],
+        [GetStorageResult],
+        ['query'],
+      ),
+    'get_tag' : IDL.Func([IDL.Text], [GetTagResult], ['query']),
+    'get_tags' : IDL.Func([], [IDL.Vec(Tag)], ['query']),
+    'get_tags_owned_by' : IDL.Func([IDL.Text], [IDL.Vec(Tag)], ['query']),
+    'is_admin' : IDL.Func([IDL.Principal], [IDL.Bool], ['query']),
+    'register_certificate' : IDL.Func([IDL.Text], [UpdateResult], []),
+    'save_certificate' : IDL.Func([IDL.Text, NFTMetadata], [UpdateResult], []),
+    'set_key' : IDL.Func([IDL.Text, IDL.Text], [UpdateResult], []),
+    'upload_media' : IDL.Func([IDL.Text, StoreArg], [UpdateResult], []),
+    'verify_tag' : IDL.Func([IDL.Text], [VerifyResult], []),
+  });
+};
+export const init = ({ IDL }) => { return []; };
