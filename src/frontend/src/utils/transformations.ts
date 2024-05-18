186e4b836b11926aa68b29a060fae6fde01e2c3d
Giacomo Chiarot <gicochiarot@gmail.com>
2024-05-18 21:55:56 +0200

    feat: Add function to convert ipfs to https

--- src/frontend/src/utils/transformations.ts
+++ src/frontend/src/utils/transformations.ts
@@ -1,11 +1,16 @@
 import {ID_LENGTH} from "./constants";
 
-
 export const intToHexId = (id: number) => {
     const hex = id.toString(16);
     return `${"0".repeat(ID_LENGTH - hex.length)}${hex}`;
 }
 
-export const reduceWalletAddress = (address: string) => {
-    return address.substring(0, 5) + "…" + address.substring(39, 42);
+export const transformUrl = (oldUrl: string) => {
+    if (oldUrl?.slice(0, 4) === "ipfs") {
+        const slices = oldUrl.split('/');
+        const ipfsHash = slices.slice(2, slices.length).join('/');
+        return `https://w3s.link/ipfs/${ipfsHash}`.replace('/ipfs/ipfs', '/ipfs');
+    } else {
+        return oldUrl;
+    }
 }
