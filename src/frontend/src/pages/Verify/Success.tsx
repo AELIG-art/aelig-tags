c271058ed3c05c167f3c24eff1e83957b681dcf8
Giacomo Chiarot <gicochiarot@gmail.com>
2024-05-18 21:56:40 +0200

    fix: Show content for frame tags too

--- src/frontend/src/pages/Verify/Success.tsx
+++ src/frontend/src/pages/Verify/Success.tsx
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from "react";
-import {Certificate, Frame } from "../../declarations/backend/backend.did";
+import {Certificate, Frame, NFTMetadata} from "../../declarations/backend/backend.did";
+import {getMetadataFromNft} from "../../utils/evm";
+import {transformUrl} from "../../utils/transformations";
 
 const Success = (props: {
     tagContent: undefined|{ Frame: Frame }|{ Certificate: Certificate }
@@ -7,40 +9,59 @@ const Success = (props: {
     const {tagContent} = props;
     const [isCertificate, setIsCertificate] = useState(false);
     const [
-        content,
-        setContent
-    ] = useState<Frame|Certificate|undefined>(undefined);
+        metadata,
+        setMetadata
+    ] = useState<undefined|NFTMetadata>();
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         if (tagContent) {
             setIsCertificate("Certificate" in tagContent);
             if ("Certificate" in tagContent) {
-                setContent(tagContent.Certificate);
+                setMetadata(tagContent.Certificate.metadata[0]);
+                setIsLoading(false);
             } else {
-                setContent(tagContent.Frame);
+                if (tagContent.Frame.nft.length === 1) {
+                    getMetadataFromNft(tagContent.Frame.nft[0])
+                        .then((res) => {
+                            setMetadata(res);
+                            setIsLoading(false);
+                        })
+                        .catch(() => {
+                            setMetadata(undefined);
+                            setIsLoading(false);
+                        });
+                } else {
+                    setMetadata(undefined);
+                    setIsLoading(false);
+                }
             }
         }
     }, [tagContent]);
 
-    return <div className="container d-flex flex-column justify-content-center text-break">
-        <div className="mt-3 mb-3">
-            {
-                isCertificate ? <div className={"row"}>
-                    <div className="col-sm-8 col-md-6 col-lg-4 mx-auto p-3">
-                        <img
-                            src={(content as Certificate|undefined)?.metadata[0]!.image}
-                            alt="Certificate"
-                            className={"img-fluid w-100 h-100"}
-                        />
-                    </div>
-                </div> : null
-            }
-        </div>
+
+    return isLoading ? <p>Loading…</p> : <div className="container d-flex flex-column justify-content-center text-break">
+        {metadata ? <div className="mt-3 mb-3">
+            <div className={"row"}>
+                <div className="col-sm-8 col-md-6 col-lg-4 mx-auto p-3">
+                    <img
+                        src={transformUrl(metadata?.image || "")}
+                        alt="artwork"
+                        className={"img-fluid w-100 h-100"}
+                    />
+                </div>
+            </div>
+        </div> : null}
         <h2>{isCertificate ? "Certificate" : "Frame"}</h2>
-        <h5>Name</h5>
-        <p>{(content as Certificate|undefined)?.metadata[0]!.name}</p>
-        <h5>Description</h5>
-        <p>{(content as Certificate|undefined)?.metadata[0]!.description}</p>
+        {
+            metadata ? <div>
+                <h5>Name</h5>
+                <p>{metadata?.name}</p>
+                <h5>Description</h5>
+                <p>{metadata?.description}</p>
+            </div> : <p>The frame is empty.</p>
+        }
+
     </div>;
 }
 
