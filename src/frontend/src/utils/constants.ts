652b1976d79a8bdf813828bd39f064d744a2e300
Giacomo Chiarot <gicochiarot@gmail.com>
2024-11-16 19:15:26 +0100

    feat: add `CHAIN_MAPPING_OPENSEA` constant

--- src/frontend/src/utils/constants.ts
+++ src/frontend/src/utils/constants.ts
@@ -1,3 +1,8 @@
 export const ID_LENGTH = 14;
 export const APP_NAME = "AELIG tags";
 export const WALLET_CONNECT_PROJECT_ID = "c51b8581597710e6b8c9f43bbc31f2d5";
+
+export const CHAIN_MAPPING_OPENSEA = {
+    'eth': 'ethereum',
+    'polygon': 'matic',
+}
