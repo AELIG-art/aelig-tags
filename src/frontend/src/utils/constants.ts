929e5a774b6946b86d04689821abad20e328bd17
Giacomo Chiarot <gicochiarot@gmail.com>
2024-11-16 19:15:10 +0100

    fix: remove unused constants

--- src/frontend/src/utils/constants.ts
+++ src/frontend/src/utils/constants.ts
@@ -1,7 +1,3 @@
-export const NETWORK = process.env.DFX_NETWORK || (process.env.NODE_ENV === "production" ? "ic" : "local");
-export const INTERNET_IDENTITY_URL = NETWORK === "local" ?
-    `http://localhost:4943/?canisterId=${process.env.INTERNET_IDENTITY_CANISTER_ID}` :
-    `https://identity.ic0.app`;
 export const ID_LENGTH = 14;
 export const APP_NAME = "AELIG tags";
 export const WALLET_CONNECT_PROJECT_ID = "c51b8581597710e6b8c9f43bbc31f2d5";
