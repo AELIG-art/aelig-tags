ae869468f5a3db35626c0a95ae2707433fdee88a
Giacomo Chiarot <gicochiarot@gmail.com>
2025-02-01 14:56:52 +0100

    feat: add `SUPPORTED_CHAINS_FOR_SELECT`

--- src/frontend/src/utils/constants.ts
+++ src/frontend/src/utils/constants.ts
@@ -1,3 +1,5 @@
+import {SupportedChain} from "./types";
+
 export const ID_LENGTH = 14;
 export const APP_NAME = "AELIG tags";
 export const WALLET_CONNECT_PROJECT_ID = "c51b8581597710e6b8c9f43bbc31f2d5";
@@ -6,3 +8,17 @@ export const CHAIN_MAPPING_OPENSEA = {
     'eth': 'ethereum',
     'polygon': 'matic',
 }
+
+export const SUPPORTED_CHAINS_FOR_SELECT: {
+  label: string,
+  value: SupportedChain
+}[] = [
+    {
+        label: 'Ethereum',
+        value: 'eth'
+    },
+    {
+        label: 'Polygon',
+        value: 'polygon',
+    }
+]
