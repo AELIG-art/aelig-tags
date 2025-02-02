55b62a1dfe05c45677518b262d6c860c3e8dcadc
Giacomo Chiarot <gicochiarot@gmail.com>
2025-02-02 21:07:43 +0100

    feat: add messages for non logged users or not owning the tag

--- src/frontend/src/pages/Tag/Tag.tsx
+++ src/frontend/src/pages/Tag/Tag.tsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { TagExpanded } from '../../utils/types';
 import './styles.Tag.css';
-import { useSiweIdentity } from 'ic-use-siwe-identity';
-import { useBackendActor } from '../../contexts/BackendActorContext';
 import { GetTagResult } from '../../declarations/backend/backend.did';
 import Certificate from './Certificate';
 import Frame from './Frame';
+import { backend } from '../../declarations/backend';
+import { useSiweIdentity } from 'ic-use-siwe-identity';
 
 type ContentProps = {
   isCertificate: boolean;
@@ -18,42 +18,36 @@ const Content = ({ isCertificate, id }: ContentProps) =>
 
 const Tag = () => {
   let { id } = useParams();
-  const { backendActor } = useBackendActor();
   const navigate = useNavigate();
-  const { isInitializing, identityAddress } = useSiweIdentity();
+  const { identityAddress } = useSiweIdentity();
   const [isLoading, setIsLoading] = useState(true);
 
   const [tag, setTag] = useState<undefined | TagExpanded>();
   const [isCertificate, setIsCertificate] = useState(false);
 
   useEffect(() => {
-    if (identityAddress === undefined && !isInitializing) {
-      navigate('/');
-    }
-    if (id && identityAddress && backendActor) {
-      backendActor.get_tag(id).then((tagRes) => {
+    if (id) {
+      backend.get_tag(id).then((tagRes) => {
         const tagResTyped = tagRes as GetTagResult;
         if ('Ok' in tagResTyped) {
-          if (tagResTyped.Ok.owner === identityAddress) {
-            setIsCertificate(tagResTyped.Ok.is_certificate);
-            setTag(tagResTyped.Ok);
-            setIsLoading(false);
-          } else {
-            navigate('/');
-          }
+          setIsCertificate(tagResTyped.Ok.is_certificate);
+          setTag(tagResTyped.Ok);
+          setIsLoading(false);
         } else {
-          navigate('/');
+          // todo: show tag does not exist error
         }
       });
     }
-  }, [id, identityAddress, isInitializing, navigate, backendActor]);
+  }, [id, navigate]);
 
   return (
     <div className={'pb-3'}>
       <h1 className="mt-5">{tag?.short_id || tag?.id}</h1>
-      <Link to={isCertificate ? '/' : '/#frames'} className="back">
-        Back
-      </Link>
+      {Boolean(identityAddress) && (
+        <Link to={isCertificate ? '/' : '/#frames'} className="back">
+          Tags list
+        </Link>
+      )}
       {!isLoading && <Content isCertificate={isCertificate} id={id!} />}
     </div>
   );
