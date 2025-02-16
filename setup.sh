d37a533e865c42152800ed225d0c71ff45ac6be8
Giacomo Chiarot <gicochiarot@gmail.com>
2025-02-16 18:09:25 +0100

    feat: add script to automatically deploy when code is merged to main; remove .sh file from formatter

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 network="local"
-tag=""
 
 usage() {
     echo "Usage: $0 -n <network_name> -t <tag_key>"
     echo "Options:"
     echo "  -n, --network <network_name>    Specify the network name (e.g., 'local', 'ic')."
-    echo "  -t, --tag <tag_key>             Specify the mandatory tag key."
     echo "  -h, --help                      Show this help message and exit."
     exit 1
 }
@@ -20,17 +18,19 @@ else
 fi
 
 if [ -z "$IC_SIWE_PROVIDER_SALT" ]; then
-    echo "The variable IC_SIWE_PROVIDER_SALT is not set in the .env file."
+    echo "The variable `IC_SIWE_PROVIDER_SALT` is not set in the .env file."
     exit 1
 fi
 
-while getopts ":n:t:h" opt; do
+if [ -z "$TAG_KEY" ]; then
+    echo "The variable `TAG_KEY` is not set in the .env file."
+    exit 1
+fi
+
+while getopts ":n:h" opt; do
     case ${opt} in
         n )
             network=$OPTARG
-            ;;
-        t )
-            tag=$OPTARG
             ;;
         h )
             usage
@@ -46,15 +46,6 @@ while getopts ":n:t:h" opt; do
     esac
 done
 shift $((OPTIND -1))
-
-if [[ -z "$tag" ]]; then
-    echo "The --tag argument was not provided. Please enter a tag key:"
-    read tag
-    if [[ -z "$tag" ]]; then
-        echo "No tag key provided, cannot proceed."
-        exit 1
-    fi
-fi
 
 echo "--- Creating canisters ---"
 dfx canister create --all -q
@@ -90,7 +81,7 @@ dfx canister call assets authorize "(principal \"$backend_canister_id\")"
 dfx canister call --ic assets grant_permission "(record{permission=variant {Commit};to_principal=principal \"$backend_canister_id\"})"
 
 echo "--- Setting backend tag key ---"
-dfx canister call backend set_key "(\"TAG_KEY\",\"$tag\")"
+dfx canister call backend set_key "(\"TAG_KEY\",\"$TAG_KEY\")"
 
 echo "--- Setting siwe id key ---"
 dfx canister call backend set_key "(\"SIWE\",\"$ic_siwe_provider_canister_id\")"
