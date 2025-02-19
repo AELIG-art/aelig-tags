d70ce0ea9fdbcd829c71da723da6dafa659cf0ae
Giacomo Chiarot <gicochiarot@gmail.com>
2025-02-19 22:54:01 +0100

    fix: remove .toml files from linter

--- src/frontend/src/declarations/backend/backend.did.js
+++ src/frontend/src/declarations/backend/backend.did.js
@@ -1,106 +1,108 @@
 export const idlFactory = ({ IDL }) => {
-  const ErrorReason = IDL.Record({ msg: IDL.Text });
+  const ErrorReason = IDL.Record({ 'msg' : IDL.Text });
   const Error = IDL.Variant({
-    NotFound: ErrorReason,
-    PermissionDenied: ErrorReason,
-    Validation: ErrorReason,
+    'NotFound' : ErrorReason,
+    'PermissionDenied' : ErrorReason,
+    'Validation' : ErrorReason,
   });
-  const UpdateResult = IDL.Variant({ Ok: IDL.Text, Err: Error });
+  const UpdateResult = IDL.Variant({ 'Ok' : IDL.Text, 'Err' : Error });
   const Tag = IDL.Record({
-    id: IDL.Text,
-    owner: IDL.Text,
-    is_certificate: IDL.Bool,
-    short_id: IDL.Text,
+    'id' : IDL.Text,
+    'owner' : IDL.Text,
+    'is_certificate' : IDL.Bool,
+    'short_id' : IDL.Text,
   });
-  const Attribute = IDL.Record({ trait_type: IDL.Text, value: IDL.Text });
+  const Attribute = IDL.Record({ 'trait_type' : IDL.Text, 'value' : IDL.Text });
   const NFTMetadata = IDL.Record({
-    name: IDL.Text,
-    description: IDL.Text,
-    attributes: IDL.Vec(Attribute),
-    image: IDL.Text,
+    'name' : IDL.Text,
+    'description' : IDL.Text,
+    'attributes' : IDL.Vec(Attribute),
+    'image' : IDL.Text,
   });
   const NFTDetails = IDL.Record({
-    id: IDL.Text,
-    chain: IDL.Text,
-    address: IDL.Text,
+    'id' : IDL.Text,
+    'chain' : IDL.Text,
+    'address' : IDL.Text,
   });
   const Certificate = IDL.Record({
-    id: IDL.Text,
-    metadata: IDL.Opt(NFTMetadata),
-    author: IDL.Text,
-    nft_details: IDL.Opt(NFTDetails),
-    short_id: IDL.Text,
-    registered: IDL.Bool,
+    'id' : IDL.Text,
+    'metadata' : IDL.Opt(NFTMetadata),
+    'author' : IDL.Text,
+    'nft_details' : IDL.Opt(NFTDetails),
+    'short_id' : IDL.Text,
+    'registered' : IDL.Bool,
   });
   const GetCertificateResult = IDL.Variant({
-    Ok: Certificate,
-    Err: Error,
+    'Ok' : Certificate,
+    'Err' : Error,
   });
   const GetCertificatesResult = IDL.Variant({
-    Ok: IDL.Vec(Certificate),
-    Err: Error,
+    'Ok' : IDL.Vec(Certificate),
+    'Err' : Error,
   });
   const NFT = IDL.Record({
-    id: IDL.Text,
-    chain: IDL.Text,
-    contract_address: IDL.Text,
+    'id' : IDL.Text,
+    'chain' : IDL.Text,
+    'contract_address' : IDL.Text,
   });
   const Lending = IDL.Record({
-    expire_timestamp: IDL.Nat,
-    to_address: IDL.Text,
+    'expire_timestamp' : IDL.Nat,
+    'to_address' : IDL.Text,
   });
   const Frame = IDL.Record({
-    id: IDL.Text,
-    nft: IDL.Opt(NFT),
-    lending: IDL.Opt(Lending),
+    'id' : IDL.Text,
+    'nft' : IDL.Opt(NFT),
+    'lending' : IDL.Opt(Lending),
   });
-  const FrameResult = IDL.Variant({ Ok: Frame, Err: Error });
-  const GetFramesResult = IDL.Variant({ Ok: IDL.Vec(Frame), Err: Error });
+  const FrameResult = IDL.Variant({ 'Ok' : Frame, 'Err' : Error });
+  const GetFramesResult = IDL.Variant({ 'Ok' : IDL.Vec(Frame), 'Err' : Error });
   const LastStorageResult = IDL.Variant({
-    Ok: IDL.Principal,
-    Err: Error,
+    'Ok' : IDL.Principal,
+    'Err' : Error,
   });
-  const GetStorageResult = IDL.Variant({ Ok: IDL.Principal, Err: Error });
-  const GetTagResult = IDL.Variant({ Ok: Tag, Err: Error });
-  const GetTagsResult = IDL.Variant({ Ok: IDL.Vec(Tag), Err: Error });
+  const GetStorageResult = IDL.Variant({ 'Ok' : IDL.Principal, 'Err' : Error });
+  const GetTagResult = IDL.Variant({ 'Ok' : Tag, 'Err' : Error });
+  const GetTagsResult = IDL.Variant({ 'Ok' : IDL.Vec(Tag), 'Err' : Error });
   const StoreArg = IDL.Record({
-    key: IDL.Text,
-    content: IDL.Vec(IDL.Nat8),
-    sha256: IDL.Opt(IDL.Vec(IDL.Nat8)),
-    content_type: IDL.Text,
-    content_encoding: IDL.Text,
+    'key' : IDL.Text,
+    'content' : IDL.Vec(IDL.Nat8),
+    'sha256' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+    'content_type' : IDL.Text,
+    'content_encoding' : IDL.Text,
   });
   const VerifyResult = IDL.Variant({
-    Ok: IDL.Variant({ Frame: Frame, Certificate: Certificate }),
-    Err: Error,
+    'Ok' : IDL.Variant({ 'Frame' : Frame, 'Certificate' : Certificate }),
+    'Err' : Error,
   });
   return IDL.Service({
-    add_storage_canister: IDL.Func([IDL.Principal], [UpdateResult], []),
-    add_tag: IDL.Func([IDL.Text, Tag], [UpdateResult], []),
-    clean_frame: IDL.Func([IDL.Text], [UpdateResult], []),
-    get_certificate: IDL.Func([IDL.Text], [GetCertificateResult], []),
-    get_certificates: IDL.Func([], [GetCertificatesResult], []),
-    get_frame: IDL.Func([IDL.Text], [FrameResult], ['query']),
-    get_frames: IDL.Func([], [GetFramesResult], []),
-    get_last_storage_principal: IDL.Func([], [LastStorageResult], ['query']),
-    get_storage_principal: IDL.Func([IDL.Text], [GetStorageResult], ['query']),
-    get_tag: IDL.Func([IDL.Text], [GetTagResult], ['query']),
-    get_tags: IDL.Func([], [GetTagsResult], ['query']),
-    is_admin: IDL.Func([IDL.Principal], [IDL.Bool], ['query']),
-    lend_frame: IDL.Func([IDL.Text, IDL.Text, IDL.Nat], [UpdateResult], []),
-    register_certificate: IDL.Func([IDL.Text], [UpdateResult], []),
-    save_certificate: IDL.Func(
-      [IDL.Text, NFTMetadata, IDL.Opt(NFTDetails)],
-      [UpdateResult],
-      []
-    ),
-    set_key: IDL.Func([IDL.Text, IDL.Text], [UpdateResult], []),
-    set_nft_on_frame: IDL.Func([IDL.Text, NFT], [UpdateResult], []),
-    transfer_frame: IDL.Func([IDL.Text, IDL.Text], [UpdateResult], []),
-    upload_media: IDL.Func([IDL.Text, StoreArg], [UpdateResult], []),
-    verify_tag: IDL.Func([IDL.Text], [VerifyResult], []),
+    'add_storage_canister' : IDL.Func([IDL.Principal], [UpdateResult], []),
+    'add_tag' : IDL.Func([IDL.Text, Tag], [UpdateResult], []),
+    'clean_frame' : IDL.Func([IDL.Text], [UpdateResult], []),
+    'get_certificate' : IDL.Func([IDL.Text], [GetCertificateResult], []),
+    'get_certificates' : IDL.Func([], [GetCertificatesResult], []),
+    'get_frame' : IDL.Func([IDL.Text], [FrameResult], ['query']),
+    'get_frames' : IDL.Func([], [GetFramesResult], []),
+    'get_last_storage_principal' : IDL.Func([], [LastStorageResult], ['query']),
+    'get_storage_principal' : IDL.Func(
+        [IDL.Text],
+        [GetStorageResult],
+        ['query'],
+      ),
+    'get_tag' : IDL.Func([IDL.Text], [GetTagResult], ['query']),
+    'get_tags' : IDL.Func([], [GetTagsResult], ['query']),
+    'is_admin' : IDL.Func([IDL.Principal], [IDL.Bool], ['query']),
+    'lend_frame' : IDL.Func([IDL.Text, IDL.Text, IDL.Nat], [UpdateResult], []),
+    'register_certificate' : IDL.Func([IDL.Text], [UpdateResult], []),
+    'save_certificate' : IDL.Func(
+        [IDL.Text, NFTMetadata, IDL.Opt(NFTDetails)],
+        [UpdateResult],
+        [],
+      ),
+    'set_key' : IDL.Func([IDL.Text, IDL.Text], [UpdateResult], []),
+    'set_nft_on_frame' : IDL.Func([IDL.Text, NFT], [UpdateResult], []),
+    'transfer_frame' : IDL.Func([IDL.Text, IDL.Text], [UpdateResult], []),
+    'upload_media' : IDL.Func([IDL.Text, StoreArg], [UpdateResult], []),
+    'verify_tag' : IDL.Func([IDL.Text], [VerifyResult], []),
   });
 };
-export const init = ({ IDL }) => {
-  return [];
-};
+export const init = ({ IDL }) => { return []; };
